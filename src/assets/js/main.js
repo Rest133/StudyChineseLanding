03a128d734af81693f049cd3bb1fcd21369572aa
Rest133 <31849309+Rest133@users.noreply.github.com>
2021-02-23 23:19:40 +0300

    Revisions

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,7 +5,10 @@ window.addEventListener("DOMContentLoaded", () => {
     let allNextButtons = document.querySelector('.content').querySelectorAll('.change-block-btn_next');
     let allPrevButtons = document.querySelector('.content').querySelectorAll('.change-block-btn_prev');
     let indexActiveBlock = 1
-    let freeWatch = false
+
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
 
     allBlocks.forEach((block, i) => {
         if (block.querySelector('.inner-block_tower') !== null) {
@@ -17,19 +20,76 @@ window.addEventListener("DOMContentLoaded", () => {
             })
 
             block.querySelector('.inner-block_tower').addEventListener('click', event => {
-                if (freeWatch) {
+                if (window.matchMedia('(min-width: 768px)').matches) {
                     indexActiveBlock = i
                     chooseActiveBlock()
-                    block.querySelectorAll('.change-block-btn').forEach(btn => btn.classList.remove('change-block-btn__fixed'))
                 }
+
                 if (window.matchMedia('(max-width: 767px)').matches) {
                     indexActiveBlock = i
                     chooseActiveMobileBlock()
-                    scrollToBlockMobile()
+                    scrollToBlock(-50)
                 }
             })
+            createSliderInBlocks(block)
         }
     })
+
+    function createSliderInBlocks(block) {
+        let allSlides = block.querySelectorAll('.inner-block__slide')
+
+        if (allSlides !== null && allSlides.length > 1) {
+            let currentPagination = createPagination(block, allSlides.length),
+                allPaginationButtons = currentPagination.querySelectorAll('.inner-block__pagination-button')
+
+            allPaginationButtons[0].classList.add('inner-block__pagination-button_active')
+
+            allSlides.forEach((slide, i) => {
+                if (!allPaginationButtons[i].classList.contains('inner-block__pagination-button_active')) {
+                    slide.classList.add('inner-block__slide_hide')
+                }
+            })
+
+            allPaginationButtons.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    allSlides.forEach((slide, i) => {
+                        if (!allPaginationButtons[i].classList.contains('inner-block__pagination-button_active')) {
+                            slide.style.animation = 'hide 3s'
+                            slide.classList.add('inner-block__slide_hide')
+                        } else {
+                            slide.style.animation = 'show 3s'
+                            slide.classList.remove('inner-block__slide_hide')
+                        }
+                    })
+                })
+            })
+
+        }
+    }
+
+    function createPagination(parentBlock, length) {
+        let paginationInnerHTML = '',
+            pagination = parentBlock.querySelector('.inner-block__pagination'),
+            allPaginationButtons
+        for (let i = 0; i < length; i++) {
+            paginationInnerHTML += '<button class="inner-block__pagination-button"/>'
+        }
+
+        pagination.innerHTML = paginationInnerHTML
+
+        allPaginationButtons = pagination.querySelectorAll('.inner-block__pagination-button')
+
+        allPaginationButtons.forEach(btn => {
+            btn.addEventListener('click', () => {
+                allPaginationButtons.forEach(button => {
+                    button.classList.remove('inner-block__pagination-button_active')
+                })
+                btn.classList.add('inner-block__pagination-button_active')
+            })
+        })
+
+        return pagination
+    }
 
     function chooseActiveMobileBlock() {
         allBlocks.forEach((block, i) => {
@@ -39,8 +99,8 @@ window.addEventListener("DOMContentLoaded", () => {
                 block.classList.add('block_active-mobile')
                 block.classList.remove('block_hide')
             }
-            if (block.querySelector('.inner-block__pagination')!==null) {
-                block.querySelector('.inner-block__pagination').parentNode.insertBefore(block.querySelector('.inner-block__pagination'),block.querySelector('.inner-block__row'))
+            if (block.querySelector('.inner-block__pagination') !== null) {
+                block.querySelector('.inner-block__pagination').parentNode.insertBefore(block.querySelector('.inner-block__pagination'), block.querySelector('.inner-block__row'))
             }
         })
     }
@@ -48,32 +108,20 @@ window.addEventListener("DOMContentLoaded", () => {
     function chooseActiveBlock() {
         allBlocks.forEach((block, i) => {
             block.classList.add('block_hide')
-            if (block.querySelector('.change-block-btn_next') !== null) {
-                block.querySelector('.change-block-btn_next').classList.remove('change-block-btn__fixed')
-            }
-            if (block.querySelector('.change-block-btn_prev') !== null) {
-                block.querySelector('.change-block-btn_prev').classList.remove('change-block-btn__fixed')
-            }
 
             if (indexActiveBlock === i) {
                 block.classList.remove('block_hide')
                 if (block.querySelector('.change-block-btn_next') !== null) {
-                    block.querySelector('.change-block-btn_next').classList.add('change-block-btn__fixed')
+                    block.querySelector('.change-block-btn_next').classList.add('change-block-btn__absolute')
                 }
                 if (block.querySelector('.change-block-btn_prev') !== null) {
-                    block.querySelector('.change-block-btn_prev').classList.add('change-block-btn__fixed')
+                    block.querySelector('.change-block-btn_prev').classList.add('change-block-btn__absolute')
+                }
+                if (block.querySelector('.tower-inner-image') !== null) {
+                    block.querySelector('.tower-inner-image').style.opacity = 1
                 }
             }
         })
-    }
-
-    function scrollToBlockMobile() {
-        $('html, body').animate({
-            scrollTop: $(allBlocks[indexActiveBlock]).offset().top - 50
-        }, {
-            duration: 1000,
-            easing: 'swing'
-        });
     }
 
     function scrollToBlock(xOffset = 0) {
@@ -83,35 +131,31 @@ window.addEventListener("DOMContentLoaded", () => {
             duration: 1000,
             easing: 'swing'
         });
-        if (indexActiveBlock === allBlocks.length - 1 && !freeWatch) {
-            document.body.style.overflow = 'auto';
-            freeWatch = true
-        } else {
-            document.body.style.overflow = 'hidden';
-            freeWatch = false
-        }
     }
 
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
+    function firstScroll() {
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            indexActiveBlock = 9
+            chooseActiveBlock()
+            document.body.style.overflow = 'hidden'
+        }
+
+        $('html, body').animate({
+            scrollTop: $(allBlocks[9]).offset().top - 250
+        }, {
+            duration: 7000,
+            easing: 'swing'
+        });
+
+        document.body.style.overflow = 'auto'
     }
 
-    if (window.matchMedia('(min-width: 768px)').matches) document.body.style.overflow = 'hidden';
+    window.addEventListener('scroll', firstScroll, {once: true})
 
-    document.querySelector('.saying-block__btn').addEventListener('click', function () {
-        if (!freeWatch && window.matchMedia('(min-width: 768px)').matches) {
-            indexActiveBlock = 1
-            scrollToBlock()
-            setTimeout(chooseActiveBlock, 500)
-        } else {
-            $('html, body').animate({
-                scrollTop: $(allBlocks[1]).offset().top - 120
-            }, {
-                duration: 1000,
-                easing: 'swing'
-            });
-        }
-    });
+    document.querySelector('.saying-block__btn').addEventListener('click', () => {
+        window.removeEventListener('scroll', firstScroll)
+        firstScroll()
+    }, {once: true});
 
     allNextButtons.forEach((btn, i) => {
         if (window.matchMedia('(max-width: 767px)').matches) {
@@ -119,7 +163,7 @@ window.addEventListener("DOMContentLoaded", () => {
             btn.addEventListener('click', function () {
                 indexActiveBlock++
                 chooseActiveMobileBlock()
-                scrollToBlockMobile()
+                scrollToBlock(-50)
             })
         } else {
             btn.textContent = allBlocks[i + 2].querySelector('.inner-block__title').textContent
@@ -141,7 +185,7 @@ window.addEventListener("DOMContentLoaded", () => {
             btn.addEventListener('click', function () {
                 indexActiveBlock--
                 chooseActiveMobileBlock()
-                scrollToBlockMobile()
+                scrollToBlock(-50)
             })
         } else {
             btn.textContent = allBlocks[i + 1].querySelector('.inner-block__title').textContent
